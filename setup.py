aac9bd0d546d20c92dfd69f5de9fef5987d9af22
LGRY-chan <bigwings1854@gmail.com>
2026-05-09 18:08:31 +0900

    Bump version to 0.1.1 and add .gitignore

--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="minimessage-rich",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
         "rich>=10.0.0",
     ],
     author="LGRY",
+    author_email="your-email@example.com",  # Placeholder
     description="A Rich extension for parsing Minecraft's MiniMessage format",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LGRY/Minimessagefied-Rich",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     python_requires=">=3.7",
 )
